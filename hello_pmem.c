4430e56c626bc116998ece334b362b2be375e244
A921 <igormartinscnd@gmail.com>
2025-06-18 15:08:25 -0400

    code cleanup

--- hello_pmem.c
+++ hello_pmem.c
@@ -9,19 +9,20 @@
 #include <string.h>
 #include <libpmem.h>
 
-/* using 4k of pmem for this example */
+// PM size of 4k
 #define PMEM_LEN 4096
 
 #define PATH "./pmemtest"
 
+// TODO: find way to safely read data from a pm pointer
 int main(int argc, char *argv[])
 {
 	char *pmemaddr;
 	size_t mapped_len;
 	int is_pmem;
 
-	/* create a pmem file and memory map it */
-	char *str = "hello, world!!!!!\n";
+	// create a pm file and memory map it
+	char *str = "hello, world!!!!!\n\0";
 	int len = strlen(str);
 
 	if ((pmemaddr = pmem_map_file(PATH, PMEM_LEN, PMEM_FILE_CREATE,
@@ -30,20 +31,16 @@ int main(int argc, char *argv[])
 		perror("pmem_map_file");
 		exit(1);
 	}
-	char *pmem_str = pmemaddr;
-	// strcpy(pmemaddr, str);
-	// memcpy();
-	pmem_memcpy(pmem_str, str, len, 0);
 
-	// following if statement is redundant becaise pmem_memcpy will persist changes
+	pmem_memcpy(pmemaddr, str, len, 0); // 0 flag indicates persist data
+
+	// following if/else statement is redundant becaise pmem_memcpy will persist changes already
 	if (is_pmem)
 		pmem_persist(pmemaddr, len);
 	// pmem_memcpy_persist(pmemaddr, str, len);
-
 	else
 		pmem_msync(pmemaddr, len);
 
-	printf("reading contents stored in pm: %s", pmem_str);
 	/*
 	 * Delete the mappings. The region is also
 	 * automatically unmapped when the process is
